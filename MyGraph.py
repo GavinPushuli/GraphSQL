2bacd2b264a10d8a3d8cd291cf6153b1c4526d3b
GavinPushuli <66453993+GavinPushuli@users.noreply.github.com>
2020-07-14 10:23:33 +0200

    Update MyGraph.py

--- MyGraph.py
+++ MyGraph.py
@@ -2,23 +2,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pymysql
     
-db = pymysql.connect(user='root', password='Qq123456', host='127.0.0.1',database='store')
+db = pymysql.connect(user='root', password='380@MySQL', host='127.0.0.1',database='store')
 cursor = db.cursor()
 sql = "SELECT * FROM store.product"
 cursor.execute(sql)
 products = cursor.fetchall()
-product_name = []
-product_available = []
-for product in products:
-    product_name.append(product[1])
-    product_available.append(product[3])
+ItemNames = []
+ItemsAvailable = []
+for products:
+    ItemNames.append(product[1])
+    ItemsAvailable.append(product[3])
 
 y_pos = np.arange(len(product_name))
 plt.figure(figsize=(12, 8))
-plt.bar(y_pos, product_available, align='center', alpha=0.5)
-plt.xticks(y_pos, product_name, rotation=90)
+plt.bar(y_pos, ItemsAvailable, align='center', alpha=0.5)
+plt.xticks(y_pos, ItemNames, rotation=90)
 plt.ylabel('Quantity')
 plt.xlabel('Products')
-plt.title('Bar Graph of Products and Quantity')
+plt.title('Production Bar Graph')
 plt.show()
 db.close()
